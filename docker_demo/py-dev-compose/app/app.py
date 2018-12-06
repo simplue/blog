b0238e7ecca4ffab3ca983130a65e82284de6970
simplue <simplue@outlook.com>
2018-12-06 18:01:38 +0800

    * docker compose update

--- docker_demo/py-dev-compose/app/app.py
+++ docker_demo/py-dev-compose/app/app.py
@@ -1,67 +1,80 @@
-# coding:utf-8
+# -*- coding: utf-8 -*-
 
-# import pymongo
+import os
+import redis
+import pymongo
+import MySQLdb
+import datetime
 import tornado.web
+import tornado.gen
 import tornado.ioloop
 import tornado.options
 
 tornado.options.parse_command_line()
-import MySQLdb
-import logging
 
-# import pymysql
-# import records
-# # 由 compose 提供 DNS 服务
-# # client = pymongo.MongoClient('mongodemo', 27017)
-# # db = client['tornado']
-# # collection = db['test']
-# import os
-# DB_CONNECT_STRING = 'mysql+mysqldb://root:{}@mysqldemo'.format(os.environ['MYSQL_ROOT_PASSWORD'])
-# print(DB_CONNECT_STRING)
-# db = records.Database(DB_CONNECT_STRING)
-# connection = pymysql.connect(host='mysqldemo',
-#                              user='root',
-#                              password='ho',
-#                              charset='utf8mb4',
-#                              cursorclass=pymysql.cursors.DictCursor)
+try:
+    mongo = pymongo.MongoClient(
+        host=os.getenv('MONGO_HOST', 'localhost'),
+        port=int(os.getenv('MONGO_PORT', 27017)),
+    )
+except:
+    mongo = None
 
-import os
-db = MySQLdb.connect(
-    host=os.getenv('MYSQL_HOST', 'localhost'),
-    port=int(os.getenv('MYSQL_PORT', 3306)),
-    user='root',
-    passwd=os.getenv('MYSQL_ROOT_PASSWORD'),
-)
+try:
+    red = redis.Redis(
+        host=os.getenv('REDIS_HOST', 'localhost'),
+        port=int(os.getenv('REDIS_PORT', 6379)),
+    )
+except:
+    red = None
 
-# MYSQL_ROOT_PASSWORD=123456
-# # MYSQL_USER=django
-# # MYSQL_PASSWORD=secret
-# MYSQL_DATABASE=myAppDB
+try:
+    mysql = MySQLdb.connect(
+        host=os.getenv('MYSQL_HOST', 'localhost'),
+        port=int(os.getenv('MYSQL_PORT', 3306)),
+        user='root',
+        passwd=os.getenv('MYSQL_ROOT_PASSWORD'),
+        db=os.getenv('MYSQL_DATABASE'),
+    )
+except:
+    mysql = None
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        # rows = db.query('select * from active_users')  # or db.query_file('sqls/active-users.sql')
-        # exist = collection.find_one({'times': {'$gte': 0}})
-        # if exist:
-        #     collection.update({}, {'$inc': {'times': 1}})
-        # else:
-        #     collection.insert_one({'times': 2})
-        #
-        # exist = collection.find_one({'times': {'$gte': 0}})
-        # with conn.ge
-        with db.cursor() as cursor:
-            cursor.execute("SELECT VERSION()")
-            DB_VERSION = cursor.fetchone()
-            logging.info(DB_VERSION)
-            # data = r.fetchone()
-        # raise Exception
-        # 使用execute方法执行SQL语句
-        exist = {'times': '1'}
-        logging.info('info')
-        logging.error('error')
-        logging.warning('warning')
-        self.write('MYSQL VERSION: {}'.format(DB_VERSION))
-        self.finish("Hello, world {} times!".format(exist.get('times')))
+        self.write('<h1 style="color: #aaa">Hello now: {}</h1>'.format(datetime.datetime.now()))
+
+        if mysql:
+            with mysql.cursor() as cursor:
+                cursor.execute("SELECT VERSION()")
+                _VERSION = cursor.fetchone()
+
+            self.write('<h1>MYSQL VERSION: {}</h1>'.format(_VERSION[0]))
+        else:
+            self.write('<h1>MYSQL MISS</h1>')
+
+        if mongo:
+            self.write('<h1>MONGO VERSION: {}</h1>'.format(mongo.server_info().get('version')))
+        else:
+            self.write('<h1>MONGO MISS</h1>')
+
+        if red:
+            self.write('<h1>REDIS VERSION: {}</h1>'.format(red.info().get('redis_version')))
+        else:
+            self.write('<h1>REDIS MISS</h1>')
+
+
+@tornado.gen.coroutine
+def do_something(func_name):
+    print 'from {} invoke at {}'.format(func_name, datetime.datetime.now())
+
+
+@tornado.gen.coroutine
+def minute_loop():
+    while True:
+        sleep = tornado.gen.sleep(60)
+        yield do_something(minute_loop.__name__)
+        yield sleep
 
 
 def make_app():
@@ -71,6 +84,18 @@ def make_app():
 
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    make_app().listen(8888)
+    event_loop = tornado.ioloop.IOLoop.current()
+    event_loop.spawn_callback(minute_loop)
+    event_loop.start()
+'''
+docker run -d --name=netdata \
+  -p 19999:19999 \
+  -v /proc:/host/proc:ro \
+  -v /sys:/host/sys:ro \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  -e PGID=999 \
+  --cap-add SYS_PTRACE \
+  --security-opt apparmor=unconfined \
+  netdata/netdata
+'''
